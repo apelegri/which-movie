d5f44a9a12bf0717b9d565ac4ae97d1d1ad45860
adrien pelegri <pelegriadrien@gmail.com>
2018-04-20 14:33:59 +0800

    removed unnecessary blocks

--- src/app/app.js
+++ src/app/app.js
@@ -1,11 +1,5 @@
 import Vue from 'vue';
 
-const EventBus = new Vue();
-window.addEventListener('popstate', () => {
-  EventBus.$emit('navigate');
-});
-
-
 const DunkirkBlurb = { name: 'dunkirk-blurb', template: `<div>
     <h2>Dunkirk</h2>
     <p class="movies__description">Miraculous evacuation of Allied soldiers from
@@ -43,63 +37,20 @@ const routes = [ {
   {path: '/the-dark-knight-rises', component: TheDarkKnightRisesBlurb}
 ];
 
-const View = {
-  name: 'router-view',
-  template: `<component :is="currentView"></component>`,
-  data() {
-    return {
-      currentView: {}
-    }
-  },
-  created() {
-    if (this.getRouteObject() === undefined) {
-      this.currentView = {
-        template: `<h2>Not Found :(. Pick a movie from the list!</h2>`
-      };
-    } else {
-      this.currentView = this.getRouteObject().component;
-      EventBus.$on('navigate', () => {
-        this.currentView = this.getRouteObject().component;
-      });
-    }
-  },
-  methods: {
-    getRouteObject() {
-      return routes.find(
-        route => route.path === window.location.pathname
-      );
-    }
-  }
-};
-const Link = {
-  name: 'router-link',
-  props: {
-    to: {
-      type: [String],
-      required: true
-    }
-  },
-  template: `<a @click="navigate" :href="to">{{ to }}</a>`,
-    methods: {
-    navigate(evt) {
-      evt.preventDefault();
-      window.history.pushState(null, null, this.to);
-      EventBus.$emit('navigate');
-    }
-  }
-};
 
 const App = {
   name: 'App',
   template: `<div id="app">
     <div class="movies">
       <h2>Which movie?</h2>
-      <router-link to="/dunkirk"></router-link>
-      <router-link to="/interstellar"></router-link>
-      <router-link to="/the-dark-knight-rises"></router-link>
+      <router-link to="/dunkirk">/dunkirk</router-link>
+      <router-link to="/interstellar">/interstellar</router-link>
+      <router-link to="/the-dark-knight-rises">/the-dark-knight-rises</router-link>
+
       <router-view></router-view>
     </div>
   </div>`,
+
   components: {
     'router-view': View,
     'router-link': Link
